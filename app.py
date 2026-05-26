fd4eeca2cf4ad9e372d66001b4b08e663bee7161
geethaln123 <amruthagowda381@gmail.com>
2026-05-26 15:53:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,37 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
+
 CORS(app, resources={r"/*": {"origins": "*"}})
+
 client = MongoClient("mongodb+srv://geethaln:Geethaln123@cluster0.idcf5jg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 db = client["pgbooking"]
 
 collection = db["bookings"]
 
-bookings = []
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def home():
-    return "Backend Running Successfully"
+    return "Backend Running Successfully ✅"
 
-@app.route('/booking', methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def booking():
 
-    data = request.json
+    data = request.get_json()
 
     collection.insert_one(data)
 
     return jsonify({
-        "message": "Booking Added Successfully",
+        "message": "Booking Added Successfully ✅",
         "data": data
     })
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
